ee131479436654798de7cc41953dd07ed9318e75
Eunice <63760394+18283@users.noreply.github.com>
2024-10-03 12:47:39 -0400

    Add files via upload

---
+++ MetodoSimplex/src/main/java/com/mycompany/metodosimplex/MetodoSimplex.java
@@ -0,0 +1,11 @@
+package com.mycompany.metodosimplex;
+
+public class MetodoSimplex {
+
+    public static void main(String[] args) {
+        Formulario1 Formulario1Frame=new Formulario1();
+        Formulario1Frame.setVisible(true);
+        Formulario1Frame.pack();
+        Formulario1Frame.setLocationRelativeTo(null); //centro
+    }
+}
